ec1a92963d2914e4e394f8592aab289c9bb56338
Dimitrios Athanasopoulos <jimathanas0@yahoo.gr>
2026-02-28 02:58:46 +0200

    Refactor PDF input and extraction process
    Updated input PDF file and modified extraction logic. Commented out unused extractors for clarity.

--- pipeline/main.py
+++ pipeline/main.py
@@ -3,16 +3,20 @@ from LayoutDetector import LayoutDetect
 from SectionExtractor import TextExtract, TableExtract, MathExtract, VLMExtract
 
 def main():
-    input_path = "pdfs/stress_test_no_text_layer.pdf"
+    input_path = "pdfs/EPITHEWRHSIS_OIKONOMIKWN_KAI_POLITIKWN_EPISTHMWN_1946_7_removed.pdf"
 
-    analyzer = Analyze()
-    detector = LayoutDetect()
     text_extractor = TextExtract()
-    table_extractor = TableExtract()
-    math_extractor = MathExtract()
+    # table_extractor = TableExtract()
+    # math_extractor = MathExtract()
     vlm_extractor = VLMExtract()
 
-    if analyzer.has_text_layer(input_path):
+    if Analyze.is_scanned(input_path):
+        print("Scanned PDF")
+        vlm_extractor.full_extract(input_path)
+        vlm_extractor.save_full_results("output") # For visual debugging
+
+    else:
+        detector = LayoutDetect()
         layout_coordinates = detector.detect(input_path)
         text_coordinates, table_coordinates, math_coordinates = detector.filter(layout_coordinates)
         detector.save_results("output") # For visual debugging
@@ -22,22 +26,18 @@ def main():
             text_results, text_results_empty = text_extractor.extract(text_coordinates)
             text_extractor.save_results("output") # For visual debugging
 
-            # If there are empty regions detected, send them to the VLM to extract
-            if text_results_empty:
-                vlm_extractor.partial_extract(text_results_empty)
-                vlm_extractor.save_results("output") # For visual debugging
+            # # If there are empty regions detected, send them to the VLM to extract
+            # if text_results_empty:
+            #     vlm_extractor.partial_extract(text_results_empty)
+            #     vlm_extractor.save_results("output") # For visual debugging
 
-        if table_coordinates:
-            table_extractor.extract(table_coordinates)
-            table_extractor.save_results("output") # For visual debugging
+        # if table_coordinates:
+        #     table_extractor.extract(table_coordinates)
+        #     table_extractor.save_results("output") # For visual debugging
 
         # if math_coordinates:
         #     math_extractor.extract(math_coordinates)
         #     math_extractor.save_results("output") # For visual debugging
-
-    else:
-        # vlm_extractor.full_extract(input_path)
-        # vlm_extractor.save_results("output") # For visual debugging
 
 if __name__ == "__main__":
     main()
